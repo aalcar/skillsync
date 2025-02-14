b21c8797d50b5e79fdcc840513cc6d64612cf7c4
Maya Kusumakar <54426748+MayaKusumakar@users.noreply.github.com>
2025-02-13 18:06:06 -0800

    tailwind css now integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import PdfUploader from "/Users/Maya/Desktop/resume/src/PDFUploader.jsx";
-/*
+
+
+
+
+
+
 const App = () => {
   const roles = ['Software Engineer', 'Web Developer', 'Mobile Developer', 'Game Developer', 'Blockchain Developer', 'Data Scientist', 'Data Engineer', 'ML Engineer', 'DevOps Engineer', 'Cloud Architect', 'Security Engineer']
   return (
     <div>
-      <h1> Resume Analyzer </h1>
+      <h1 style={{ backgroundColor: 'lightblue', color: 'darkblue' }}> Resume Analyzer </h1>
       <ul>
         {roles.map((role, index) => (
           <li key={index}> {role} </li>
         ))}
       </ul>
+
+      <div className="flex justify-center items-center">
+      <PdfUploader />
+    </div>
     </div>
   )
 }
-*/ 
-function App() {
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-50">
-      <PdfUploader />
-    </div>
-  );
-}
-export default App
+export default App 
