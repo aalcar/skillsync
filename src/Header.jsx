f2bd6f88d62d845851b22eb62f7ab0f30c9fad89
hir-al-14 <hiral.arora1406@gmail.com>
2025-03-01 16:08:34 -0800

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Moon } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
 
-const Header = () => {
+const Header = ({ darkMode, toggleTheme }) => {
     return (
         <header style={{
-            background: 'rgb(255, 255, 255)', 
-            color: '#f8f9fa', 
+            background: darkMode ? " #1f2937" : "#ffffff",
+            color: darkMode ? " #f8f9fa" : "#374151", 
             display: 'flex',
             justifyContent: 'space-between',
             alignItems: 'center',
@@ -13,31 +13,31 @@ const Header = () => {
             fontFamily: 'Helvetica, sans-serif'
         }}>
             <h1 style={{  marginLeft: '20px', fontSize: '2rem', fontWeight: '550', fontFamily: "'Poppins', sans-serif", background: 'linear-gradient(to right, #3b82f6, #9333ea)', WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent', display: 'inline-block' }}>          
-                Name
+                SkillScalar
             </h1>
       
             <nav style={{ display: 'flex', gap: '40px' }}>
-                <a href="#" style={{ color: '#374151', textDecoration: 'none', fontSize: '1.0rem' }}>Home</a>
-                <a href="#" style={{ color: '#374151', textDecoration: 'none', fontSize: '1.0rem' }}>Recommendations</a>
-                <a href="#" style={{ color: '#374151', textDecoration: 'none', fontSize: '1.0rem' }}>Resume Analyzer</a>
+                <a href="#" style={{ color: darkMode ? "#f8f9fa" : "#374151", textDecoration: 'none', fontSize: '1.0rem' }}>Home</a>
+                <a href="#" style={{ color: darkMode ? "#f8f9fa" : "#374151", textDecoration: 'none', fontSize: '1.0rem' }}>Recommendations</a>
+                <a href="#" style={{ color: darkMode ? "#f8f9fa" : "#374151", textDecoration: 'none', fontSize: '1.0rem' }}>Resume Analyzer</a>
             </nav>
 
-            <div style={{ display: 'flex', gap: '5px', alignItems: 'left', marginRight: '15px' }}>
-                <button style={{
+            <div style={{ display: 'flex', gap: '10px', alignItems: 'left', marginRight: '15px' }}>
+                <button onClick={toggleTheme} style={{
                     width: '50px',
                     height: '50px',
                     borderRadius: '50%',
-                    background: '#ffffff',
+                    background: darkMode ? "#1f2937" : "#ffffff",
                     border: 'none',
                     display: 'flex',
                     alignItems: 'center',
                     justifyContent: 'center',
                     cursor: 'pointer'
                 }}>
-                    <Moon color="#374151" size={25} />
+                    {darkMode ? <Sun color="#9F67E0" size={25} /> : <Moon color="#374151" size={25} />}
                 </button>
-                <button type="submit" style={{ padding: '10px 20px', backgroundColor: '#fff', color: '#4285f4', border: '2px solid #4285f4',  borderRadius: '4px', cursor: 'pointer', fontWeight: 'bold', transition: 'background-color 0.3s, color 0.3s' }}>Sign up</button>
-                <button type="submit" style={{ padding: '10px 20px', backgroundColor: '#fff', color: '#4285f4', border: '2px solid #4285f4',  borderRadius: '4px', cursor: 'pointer', fontWeight: 'bold', transition: 'background-color 0.3s, color 0.3s' }}>Sign in</button>
+                <button type="submit" style={{ padding: '10px 20px', backgroundColor: darkMode ? "#1f2937" : "#fff", color: darkMode ? " #9F67E0" : "#4285f4", border: darkMode ? "2px solid #9F67E0" : "2px solid #4285f4",  borderRadius: '4px', cursor: 'pointer', fontWeight: 'bold' }}>Sign up</button>
+                <button type="submit" style={{ padding: '10px 20px', backgroundColor: darkMode ? "#1f2937" : "#fff", color: darkMode ? " #9F67E0" : "#4285f4", border: darkMode ? "2px solid #9F67E0" : "2px solid #4285f4",  borderRadius: '4px', cursor: 'pointer', fontWeight: 'bold' }}>Sign in</button>
             </div>
         </header>
     );
