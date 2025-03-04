014f47e2d0667e9e81ef63cae44fb34c9e02a880
hir-al-14 <hiral.arora1406@gmail.com>
2025-03-04 10:49:53 -0800

    Update JobSearchBar.jsx

--- src/JobSearchBar.jsx
+++ src/JobSearchBar.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import PdfUploader from "./PDFUploader.jsx";
 
 function JobSearchForm({ darkMode }) {
+  const [jobTitle, setJobTitle] = useState("");
+  const [jobType, setJobType] = useState("");
+  const [location, setLocation] = useState("");
+  const [jobResults, setJobResults] = useState([]);
+
   return (
     <form>
-        
       <div style={{ 
           display: 'flex', 
           gap: '10px', 
@@ -14,7 +18,23 @@ function JobSearchForm({ darkMode }) {
           borderRadius: '5px', 
           boxShadow: darkMode ? "0 1px 3px rgba(255, 255, 255, 0.12), 0 1px 2px rgba(255, 255, 255, 0.24)" : "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
       }}>
-        <input type="text" placeholder="Job title, keywords, or company" style={{ padding: '10px', border: '1px solid #ccc', borderRadius: '4px', flex: '1' }} />
+        <select style={{ padding: '10px', border: '1px solid #ccc', borderRadius: '4px', flex: '1' }}>
+          <option value="">Job title, keywords, or company</option>
+          <option value="Software Developer/Engineer">Software Developer/Engineer</option>
+          <option value="Data Scientist">Data Scientist</option>
+          <option value="Systems Administrator">Systems Administrator</option>
+          <option value="Network Engineer">Network Engineer</option>
+          <option value="Cybersecurity Analyst">Cybersecurity Analyst</option>
+          <option value="DevOps Engineer">DevOps Engineer</option>
+          <option value="Cloud Architect">Cloud Architect</option>
+          <option value="Artificial Intelligence Engineer">Artificial Intelligence Engineer</option>
+          <option value="Software Tester">Software Tester</option>
+          <option value="Database Administrator">Database Administrator</option>
+          <option value="App Developer">App Developer</option>
+          <option value="UX/UI Designer">UX/UI Designer</option>
+          <option value="Full Stack Developer">Full Stack Developer</option>
+          <option value="Product Manager">Product Manager</option>
+        </select>  
         <select style={{ padding: '10px', border: '1px solid #ccc', borderRadius: '4px', flex: '0.5' }}>
           <option value="">Job Type</option>
           <option value="full-time">Full-time</option>
@@ -23,7 +43,7 @@ function JobSearchForm({ darkMode }) {
           <option value="temporary">Temporary</option>
           <option value="internship">Internship</option>
         </select>                          
-        <input type="text" placeholder="Location" style={{ padding: '10px', border: '1px solid #ccc', borderRadius: '4px', flex: '1' }} />
+        <input type="text" placeholder="Location (eg. Davis, CA)" style={{ padding: '10px', border: '1px solid #ccc', borderRadius: '4px', flex: '1' }} />
         <button type="submit" style={{ padding: '10px 20px', backgroundColor: darkMode ? "#9F67E0" : "#4285f4", color: darkMode ? "#1E1B29" : "#fff", border: 'none', borderRadius: '4px', cursor: 'pointer' }}>Search</button>
       </div>
 
