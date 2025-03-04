47295517bb302d1c50cf49e09c825a6ad6f58b1e
hir-al-14 <hiral.arora1406@gmail.com>
2025-03-04 10:50:52 -0800

    Add files via upload

---
+++ src/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import JobSearchBar from "./JobSearchBar";
+import JobsForYou from "./JobsForYou";
+
+const Dashboard = ({ darkMode, toggleTheme }) => {
+  return (
+    <div>
+      <Header darkMode={darkMode} toggleTheme={toggleTheme} />
+      <JobSearchBar darkMode={darkMode} />
+      <JobsForYou /> {}
+    </div>
+  );
+};
+
+export default Dashboard;
