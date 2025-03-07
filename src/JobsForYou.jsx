bac4a8b71918d51e49b871a3bcddff2ce5e6add3
hir-al-14 <hiral.arora1406@gmail.com>
2025-03-06 16:11:45 -0800

    Update JobsForYou.jsx

--- src/JobsForYou.jsx
+++ src/JobsForYou.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
-function JobsForYou() {
+function JobsForYou({ darkMode }) {
   return (
     <div style={{
-      width: '400px', // Match Job Listings width
-      minHeight: '550px', // Match Job Listings height
-      overflowY: 'auto', // Allow scrolling if content overflows
+      width: '400px',
+      minHeight: '550px',
+      overflowY: 'auto',
       border: '1px solid #ccc',
       borderRadius: '8px',
       padding: '10px',
-      backgroundColor: 'white',
-      boxShadow: '0 4px 8px rgba(0,0,0,0.2)',
+      backgroundColor: darkMode ? '#1f2937' : 'white',  // Adjust for dark mode
+      color: darkMode ? '#fff' : '#000',  // Text color for dark mode
+      boxShadow: darkMode ? '0 4px 8px rgba(255, 255, 255, 0.2)' : '0 4px 8px rgba(0, 0, 0, 0.2)',
     }}>
       <h2 style={{ textAlign: 'left', margin: '10px', fontSize: '1.5em', fontWeight: 'bold' }}>Jobs for You</h2>
 
