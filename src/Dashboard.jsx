e46e8ef74399d0203f0b1510b3cb1f6cfd7dbe1c
hir-al-14 <hiral.arora1406@gmail.com>
2025-03-06 16:11:27 -0800

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import JobSearchBar from "./JobSearchBar";
 import JobsForYou from "./JobsForYou";
-import JobListings from "./jobListings";
+import JobListings from "./JobListings";
 
 const Dashboard = ({ darkMode, toggleTheme }) => {
   return (
@@ -12,18 +12,18 @@ const Dashboard = ({ darkMode, toggleTheme }) => {
       <JobSearchBar darkMode={darkMode} />
 
       {/* Main Content: Align "Jobs for You" and "Job Listings" */}
-      <div style={{ 
-        display: "flex", 
-        justifyContent: "center", 
-        alignItems: "flex-start", // Align at the same height
-        gap: "40px", // Space between the boxes
-        marginTop: "30px", // Adjusts space below search bar
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "flex-start",
+        gap: "40px",
+        marginTop: "30px",
       }}>
         {/* Left: Jobs for You */}
-        <JobsForYou />
+        <JobsForYou darkMode={darkMode} />
 
         {/* Right: Job Listings */}
-        <JobListings />
+        <JobListings darkMode={darkMode} />
       </div>
     </div>
   );
